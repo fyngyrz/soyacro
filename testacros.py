877344623a4491ea28644f16b7fc66907b50e155
fyngyrz <fyngyrz@gmail.com>
2018-11-29 09:31:52 -0700

    Additional tests

--- testacros.py
+++ testacros.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python
 
+# Project Info:
+# =============
+#   Written by: fyngyrz - codes with magnetic needle
+#   Incep date: November 24th, 2018
+#  Last Update: November 29th, 2018 (this code file only)
+#  Environment: Webserver cgi, HTML 4.01 strict, Python 2.7
+# Source Files: soyacro.py, acrobase.txt (these may be renamed)
+#  Tab Spacing: Set to 4 for sane readability of Python source
+#     Security: Suitable for benign users only (IOW, me.)
+#      Purpose: Creates informative <abbr> tag wraps around
+#               all-caps terms in the source text. Written
+#               to support use of <abbr> on soylentnews.org
+#               Also supports canned aa_macro styles via mfile
+#      License: None. Use as you will. PD, free, etc.
+# Dependencies: aa_webpage.py by fyngyrz
+#               aa_macro.py by fyngyrz
+#               standard Python cgi import library
+#               standard Python sys import library
+#               standard Python os import library
+# ----------------------------------------------------------
+
 afile = 'acrobase.txt'
 
 errors = ''
@@ -10,6 +31,17 @@ try:
 except:
 	print 'failed to read ' + afile
 	exit()
+
+def tstchars(text):
+	for c in text:
+		if (c >= 'A' and c <= 'Z'):
+			pass
+		else:
+			if (c >= '0' and c <= '9'):
+				pass
+			else:
+				return False
+	return True
 
 di = {}
 data = text.split('\n')
@@ -22,10 +54,13 @@ for line in data:
 			errors += 'line %d failed to split into three components\n' % (ln)
 		else:
 			if expansion.find('<') != -1: errors += 'expansion of key '+key+' contains "<"\n'
-			if expansion.find('>') != -1: errors += 'expansion of key '+key+' contains "<"\n'
+			if expansion.find('>') != -1: errors += 'expansion of key '+key+' contains ">"\n'
 			if replacement.find('<') != -1: errors += 'expansion of key '+key+' contains "<"\n'
 			if replacement.find('>') != -1: errors += 'expansion of key '+key+' contains "<"\n'
 			if expansion == '': errors += 'expansion of key '+key+' is empty\n'
+			if key.find('<') != -1: errors += 'key '+key+' contains "<"\n'
+			if key.find('>') != -1: errors += 'key '+key+' contains ">"\n'
+			if tstchars(key) == False: errors += 'key '+key+' contains illegal characters\n'
 			if key == '':
 				errors += 'key is empty at line %d\n' % (ln)
 			else:
